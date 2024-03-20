612035ee1b41545d8879122e56f05e2ba9e7f3b4
Faraz Saleem <farazkhan2062@gmail.com>
2024-03-20 09:35:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/cnv node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log("Your value is " + (answer.firstNumber + answer.SecondNumber));
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your value is " + (answer.firstNumber - answer.SecondNumber));
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Your value is " + (answer.firstNumber * answer.SecondNumber));
+}
+else if (answer.operator === "Division") {
+    console.log("Your value is " + (answer.firstNumber / answer.SecondNumber));
+}
+else {
+    console.log("Please select valid operator");
+}
