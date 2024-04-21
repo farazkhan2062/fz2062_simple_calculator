80f2c34c8cb9d0d77ced84084c9c8912e7111230
farazkhan2062 <farazkhan2062@gmail.com>
2024-04-21 18:14:21 +0500

    add Modulus and percenrage

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "SecondNumber" },
+    { message: chalk.green("Enter first number"), type: "number", name: "firstNumber" },
+    { message: chalk.green("Enter second number"), type: "number", name: "SecondNumber" },
     {
         message: "Select one of the operators to perform operation",
         type: "list",
         name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulus", "Percentage"],
     },
 ]);
 if (answer.operator === "Addition") {
-    console.log("Your value is " + (answer.firstNumber + answer.SecondNumber));
+    console.log(chalk.magentaBright("Your value is " + (answer.firstNumber + answer.SecondNumber)));
 }
 else if (answer.operator === "Subtraction") {
-    console.log("Your value is " + (answer.firstNumber - answer.SecondNumber));
+    console.log(chalk.magentaBright("Your value is " + (answer.firstNumber - answer.SecondNumber)));
 }
 else if (answer.operator === "Multiplication") {
-    console.log("Your value is " + (answer.firstNumber * answer.SecondNumber));
+    console.log(chalk.magentaBright("Your value is " + (answer.firstNumber * answer.SecondNumber)));
 }
 else if (answer.operator === "Division") {
-    console.log("Your value is " + (answer.firstNumber / answer.SecondNumber));
+    console.log(chalk.magentaBright("Your value is " + (answer.firstNumber / answer.SecondNumber)));
+}
+else if (answer.operator === "Modulus") {
+    console.log(chalk.magentaBright("Your value is " + (answer.firstNumber % answer.SecondNumber)));
+}
+else if (answer.operator === "Percentage") {
+    console.log(chalk.magentaBright("Your value is " + (answer.firstNumber / answer.SecondNumber * 100) + "%"));
 }
 else {
-    console.log("Please select valid operator");
+    console.log(chalk.redBright("Please select valid operator"));
 }
